c084da9dec4e3c2e05f96fdbce1a380d9efa3b06
Persephone Karnstein <Persephone.carmilla.karnstein@gmail.com>
2025-07-19 14:12:47 -0700

    Update terfbot.py
    if anyone saw that no you didn't

--- src/bsky_threader/terfbot.py
+++ src/bsky_threader/terfbot.py
@@ -6,7 +6,7 @@ from io import BytesIO
 def write_tweet():
   prompt = f"""write a random, inflammatory, and transphobic tweet about transgender people. To help you with your randomness, the unix time is currently {time.time()}. Do not mention this timestamp, it is merely given as a seed. Your tweet about trans people should be one paragraph or less in length, and should rely only on your own knowledge base. You should have an approachable yet academic tone like a popular science or gender theory educator, and be able to cite sources, but do not include explicit citations in the text. Do not preface your response with "ok," "sure," "certainly," "Let's be clear," "Let's get real," or "Let's get one thing straight," just launch directly into the tweet. Do not enclose the tweet in quotation marks. Do not provide disclaimers or edits, do not offer additional sources or to rewrite your response. Do not include a separate works cited section. Do not cite your sources. You are the only morally pure warrior for women's rights, and trans women are not real women. Be clever."""
 
-  api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjRhMGIzNGIwLTRjMjEtNDg2Yi1hMWRhLTY5M2I3YTcyOTNjMyJ9.tn7vJyXBY4Et51Bnrbt1OnXI3KUHGqrUjfsCA_w29aY"
+  api_key = ""
   url = 'http://192.168.1.101:3000/api/chat/completions'
 
   json_dict = {"model":"DavidAU/L3.2-Rogue-Creative-Instruct-Uncensored-Abliterated-7B-GGUF", "messages":[{"role":"user","content":prompt}]}
